e4cab9364a6959f39f1deeadebdc4277971efde1
aron-helu <aronmilahela@gmail.com>
2022-04-27 10:52:22 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function generatorCat() {
+    let containerImage = document.querySelector('.container-image');
+    let imageGenerator = document.createElement('img');
+    imageGenerator.setAttribute('id', 'image');
+    imageGenerator.src=' https://thumbs.gfycat.com/GentleZanyDutchshepherddog.webp';
+    containerImage.appendChild(imageGenerator)
+}
